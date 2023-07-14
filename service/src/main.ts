5590c9fe81d1058e9e5f615e25999ebd6257a1fe
cchenkov <chudomir.chenkov31@gmail.com>
2023-07-14 23:47:14 +0300

    build: added typeorm dependency and modified app.module.ts to include and setup TypeOrmModule via ConfigModule for reading .env config values

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const host = configService.get('SERVER_HOST') || '127.0.0.1';
+  const port = +configService.get('SERVER_PORT') || 3000;
+
+  await app.listen(port, host);
 }
 bootstrap();
