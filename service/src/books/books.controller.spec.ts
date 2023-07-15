da44e3eb9b59bf2e662a3b6e50a6468772615613
cchenkov <chudomir.chenkov31@gmail.com>
2023-07-15 20:05:41 +0300

    test: modified BooksController and BooksService tests to reflect the latest changes and added tests covering the new Http Not Found responses

--- service/src/books/books.controller.spec.ts
+++ service/src/books/books.controller.spec.ts
@@ -3,41 +3,80 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { NotFoundException } from '@nestjs/common';
 
-const mockBooksService = {
-  create: jest.fn((dto: CreateBookDto) => ({
-    id: Date.now(),
-    ...dto,
-  })),
-  findAll: jest.fn(() => [
-    {
-      title: '1984',
-      author: 'George Orwell',
-      isbn: '9780451524935',
-      price: 7.48,
-      quantity: 10,
-    },
-    {
-      title: 'Animal Farm',
-      author: 'George Orwell',
-      isbn: '9788129116123',
-      price: 6.99,
-      quantity: 15,
-    },
-  ]),
-  findOne: jest.fn((id: number) => ({
-    id,
+const books = [
+  {
+    id: 1,
     title: '1984',
     author: 'George Orwell',
     isbn: '9780451524935',
     price: 7.48,
     quantity: 10,
-  })),
-  update: jest.fn((id: number, dto: UpdateBookDto) => ({
-    id,
-    ...dto,
-  })),
-  remove: jest.fn((id: number) => id),
+  },
+  {
+    id: 2,
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    isbn: '9788129116123',
+    price: 6.99,
+    quantity: 15,
+  },
+];
+
+const createBookDto = {
+  title: '1984',
+  author: 'George Orwell',
+  isbn: '9780451524935',
+  price: 7.48,
+  quantity: 10,
+};
+
+const updateBookDto = {
+  price: 11.29,
+  quantity: 15,
+};
+
+const mockBooksService = {
+  create: jest.fn((dto: CreateBookDto) =>
+    Promise.resolve({
+      id: Date.now(),
+      ...dto,
+    }),
+  ),
+  findAll: jest.fn(() => Promise.resolve(books)),
+  findOne: jest.fn((id: number) =>
+    id === 3
+      ? Promise.reject()
+      : Promise.resolve({
+          id,
+          title: '1984',
+          author: 'George Orwell',
+          isbn: '9780451524935',
+          price: 7.48,
+          quantity: 10,
+        }),
+  ),
+  update: jest.fn((id: number, dto: UpdateBookDto) =>
+    id === 3
+      ? Promise.reject()
+      : Promise.resolve({
+          id,
+          ...dto,
+        }),
+  ),
+  remove: jest.fn((id: number) =>
+    id === 3
+      ? Promise.reject()
+      : Promise.resolve({
+          id,
+          title: '1984',
+          author: 'George Orwell',
+          isbn: '9780451524935',
+          price: 7.48,
+          quantity: 10,
+        }),
+  ),
 };
 
 describe('BooksController', () => {
@@ -61,81 +100,63 @@ describe('BooksController', () => {
 
   describe('create', () => {
     it('should create a book', async () => {
-      const createBookDto = {
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
-      };
-
-      expect(controller.create(createBookDto)).toEqual({
+      expect(await controller.create(createBookDto)).toEqual({
         id: expect.any(Number),
         ...createBookDto,
       });
-
       expect(mockBooksService.create).toHaveBeenCalledWith(createBookDto);
     });
   });
 
   describe('findAll', () => {
     it('should return list of books', async () => {
-      expect(controller.findAll()).toEqual([
-        {
-          title: '1984',
-          author: 'George Orwell',
-          isbn: '9780451524935',
-          price: 7.48,
-          quantity: 10,
-        },
-        {
-          title: 'Animal Farm',
-          author: 'George Orwell',
-          isbn: '9788129116123',
-          price: 6.99,
-          quantity: 15,
-        },
-      ]);
-
+      expect(await controller.findAll()).toEqual(books);
       expect(mockBooksService.findAll).toHaveBeenCalled();
     });
   });
 
   describe('findOne', () => {
     it('should return a single book', async () => {
-      expect(controller.findOne('1')).toEqual({
-        id: 1,
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
-      });
-
+      expect(await controller.findOne('1')).toEqual(books[0]);
       expect(mockBooksService.findOne).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw NotFoundException on non-existent id', async () => {
+      await expect(controller.findOne('3')).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(mockBooksService.findOne).toHaveBeenCalledWith(3);
     });
   });
 
   describe('update', () => {
     it('should update a book', async () => {
-      const updateBookDto = {
-        price: 11.29,
-        quantity: 15,
-      };
-
-      expect(controller.update('1', updateBookDto)).toEqual({
+      expect(await controller.update('1', updateBookDto)).toEqual({
         id: 1,
         ...updateBookDto,
       });
-
       expect(mockBooksService.update).toHaveBeenCalledWith(1, updateBookDto);
+    });
+
+    it('should throw NotFoundException on non-existent id', async () => {
+      await expect(controller.update('3', updateBookDto)).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(mockBooksService.update).toHaveBeenCalledWith(3, updateBookDto);
     });
   });
 
   describe('remove', () => {
     it('should remove a book', async () => {
-      expect(controller.remove('1')).toEqual(1);
+      expect(await controller.remove('1')).toEqual(books[0]);
       expect(mockBooksService.remove).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw NotFoundException on non-existent id', async () => {
+      await expect(controller.remove('3')).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(mockBooksService.remove).toHaveBeenCalledWith(3);
     });
   });
 });
--- service/src/books/books.service.spec.ts
+++ service/src/books/books.service.spec.ts
@@ -4,30 +4,44 @@ import { plainToInstance } from 'class-transformer';
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
+
+const books = [
+  {
+    id: 1,
+    title: '1984',
+    author: 'George Orwell',
+    isbn: '9780451524935',
+    price: 7.48,
+    quantity: 10,
+  },
+  {
+    id: 2,
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    isbn: '9788129116123',
+    price: 6.99,
+    quantity: 15,
+  },
+];
+
+const createBookDto = {
+  title: '1984',
+  author: 'George Orwell',
+  isbn: '9780451524935',
+  price: 7.48,
+  quantity: 10,
+};
+
+const updateBookDto = {
+  price: 11.29,
+  quantity: 15,
+};
 
 const mockBookRepository = {
   create: jest.fn((dto: CreateBookDto) => plainToInstance(Book, dto)),
   save: jest.fn((book: Book) => Promise.resolve(book)),
-  find: jest.fn(() =>
-    Promise.resolve([
-      {
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
-      },
-      {
-        title: 'Animal Farm',
-        author: 'George Orwell',
-        isbn: '9788129116123',
-        price: 6.99,
-        quantity: 15,
-      },
-    ]),
-  ),
-  findOneBy: jest.fn(({ id }: { id: number }) =>
+  find: jest.fn(() => Promise.resolve(books)),
+  findOneByOrFail: jest.fn(({ id }: { id: number }) =>
     Promise.resolve(
       plainToInstance(Book, {
         id,
@@ -39,18 +53,7 @@ const mockBookRepository = {
       }),
     ),
   ),
-  update: jest.fn((id: number, dto: UpdateBookDto) =>
-    Promise.resolve(
-      plainToInstance(Book, {
-        id,
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        ...dto,
-      }),
-    ),
-  ),
-  delete: jest.fn().mockResolvedValue(true),
+  remove: jest.fn((book: Book) => Promise.resolve(book)),
 };
 
 describe('BooksService', () => {
@@ -73,18 +76,9 @@ describe('BooksService', () => {
 
   describe('create', () => {
     it('should create a new book entity and return it', async () => {
-      const createBookDto = {
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
-      };
-
       const book = plainToInstance(Book, createBookDto);
 
       expect(await service.create(createBookDto)).toEqual(book);
-
       expect(mockBookRepository.create).toHaveBeenCalledWith(createBookDto);
       expect(mockBookRepository.save).toHaveBeenCalledWith(book);
     });
@@ -92,69 +86,44 @@ describe('BooksService', () => {
 
   describe('findAll', () => {
     it('should return array of books', async () => {
-      expect(await service.findAll()).toEqual([
-        {
-          title: '1984',
-          author: 'George Orwell',
-          isbn: '9780451524935',
-          price: 7.48,
-          quantity: 10,
-        },
-        {
-          title: 'Animal Farm',
-          author: 'George Orwell',
-          isbn: '9788129116123',
-          price: 6.99,
-          quantity: 15,
-        },
-      ]);
-
+      expect(await service.findAll()).toEqual(books);
       expect(mockBookRepository.find).toHaveBeenCalled();
     });
   });
 
   describe('findOne', () => {
     it('should return a single book', async () => {
-      expect(await service.findOne(1)).toEqual({
+      expect(await service.findOne(1)).toEqual(books[0]);
+      expect(mockBookRepository.findOneByOrFail).toHaveBeenCalledWith({
         id: 1,
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
       });
-
-      expect(mockBookRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
     });
   });
 
   describe('update', () => {
     it('should update a book and return it', async () => {
-      const updateBookDto = {
-        price: 11.29,
-        quantity: 15,
-      };
-
-      // service.update uses findOneBy internally
-      // result won't have any changes due to mock implementation of findOneBy
-      expect(await service.update(1, updateBookDto)).toEqual({
-        id: 1,
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '9780451524935',
-        price: 7.48,
-        quantity: 10,
+      const book = plainToInstance(Book, {
+        ...books[0],
+        ...updateBookDto,
       });
 
-      expect(mockBookRepository.update).toHaveBeenCalledWith(1, updateBookDto);
-      expect(mockBookRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(await service.update(1, updateBookDto)).toEqual(book);
+      expect(mockBookRepository.save).toHaveBeenCalledWith(book);
+      expect(mockBookRepository.findOneByOrFail).toHaveBeenCalledWith({
+        id: 1,
+      });
     });
   });
 
   describe('remove', () => {
     it('should remove a book', async () => {
-      expect(await service.remove(1)).toEqual(1);
-      expect(mockBookRepository.delete).toHaveBeenCalledWith(1);
+      const book = plainToInstance(Book, books[0]);
+
+      expect(await service.remove(1)).toEqual(book);
+      expect(mockBookRepository.remove).toHaveBeenCalledWith(book);
+      expect(mockBookRepository.findOneByOrFail).toHaveBeenCalledWith({
+        id: 1,
+      });
     });
   });
 });
