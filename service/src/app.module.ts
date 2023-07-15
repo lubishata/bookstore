1faa265555eb25414fbe83647b755bd13a2da3b2
cchenkov <chudomir.chenkov31@gmail.com>
2023-07-15 11:33:33 +0300

    feat: add book resource, including entity, DTOs, controller and service

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AppService } from './app.service';
       }),
       inject: [ConfigService],
     }),
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ service/src/books/books.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities/book.entity';
+
+@Injectable()
+export class BooksService {
+  constructor(
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
+  ) {}
+
+  create(createBookDto: CreateBookDto) {
+    const book = this.bookRepository.create(createBookDto);
+    return this.bookRepository.save(book);
+  }
+
+  findAll() {
+    return this.bookRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.bookRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateBookDto: UpdateBookDto) {
+    await this.bookRepository.update(id, updateBookDto);
+    return this.bookRepository.findOneBy({ id });
+  }
+
+  async remove(id: number) {
+    await this.bookRepository.delete(id);
+  }
+}
