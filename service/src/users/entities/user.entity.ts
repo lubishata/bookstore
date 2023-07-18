b320959f0eb908713d4922df642280ca105c48e9
cchenkov <chudomir.chenkov31@gmail.com>
2023-07-18 18:05:53 +0300

    refactor: users module redesigned to include roles with many-to-many relations and password hashing

---
+++ service/src/users/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Role } from './role.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @ManyToMany(() => Role)
+  @JoinTable()
+  roles: Role[];
+}
